fad1c2de2f6a9907c0e9e3022171754723bea5a3
omaryahia4 <omar.yahia4444@gmail.com>
2021-08-11 21:15:09 +0100

    airbnb

---
+++ tests/test_console.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines unittests for console.py."""
+import os
+import pep8
+import unittest
+import models
+from unittest.mock import patch
+from io import StringIO
+from console import HBNBCommand
+from models.engine.db_storage import DBStorage
+from models.engine.file_storage import FileStorage
+
+class TestHBNBCommand(unittest.TestCase):
+    """Unittests for testing the HBNB command interpreter."""
+    
