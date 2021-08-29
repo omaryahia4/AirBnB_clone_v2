c97bc7c603babe41b55924dbe57723960c7c180b
omaryahia4 <omar.yahia4444@gmail.com>
2021-08-29 15:23:21 +0100

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,8 +7,8 @@ mkdir -p  /data/web_static/
 mkdir -p /data/web_static/releases/test/
 mkdir /data/web_static/shared/
 sudo echo "Hello AirBnb" | tee /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test /data/web_static/current 
+ln -sf /data/web_static/releases/test /data/web_static/current
 chown -R -h ubuntu:ubuntu  /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' 
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n'
 /etc/nginx/sites-available/default
 sudo service nginx restart
