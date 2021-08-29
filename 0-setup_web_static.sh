fa9c5ca48491cdf31429e2060e188c6d92c8c01c
omaryahia4 <omar.yahia4444@gmail.com>
2021-08-29 15:21:27 +0100

    Prepare your web servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Prepare your web servers
+sudo apt-get update -y
+sudo apt-get updgrade -y
+sudo apt-get install -y nginx
+mkdir -p  /data/web_static/
+mkdir -p /data/web_static/releases/test/
+mkdir /data/web_static/shared/
+sudo echo "Hello AirBnb" | tee /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test /data/web_static/current 
+chown -R -h ubuntu:ubuntu  /data/
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' 
+/etc/nginx/sites-available/default
+sudo service nginx restart
