e5bb7942b7264af9f5b01f926f1fa7088f4446c3
omaryahia4 <omar.yahia4444@gmail.com>
2021-09-12 11:09:36 +0100

    current state

--- models/state.py
+++ models/state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from models.engine.file_storage import FileStorage
 from models.city import City
 import models
 from models.base_model import BaseModel, Base
@@ -13,10 +14,11 @@ class State(BaseModel, Base):
     __tablename__ = "states"
     cities = relationship("City",  backref="state")
 
-    @property
-    def cities():
-        city_list = []
-        for city in models.storage.all(City).values:
-            if city.state_id == city.id:
-                city_list.append(city)
-        return city_list
+    if models.storage == FileStorage():
+        @property
+        def cities(self):
+            city_list = []
+            for city in models.storage.all(City).values:
+                if city.state_id == self.city.id:
+                    city_list.append(city)
+            return city_list
