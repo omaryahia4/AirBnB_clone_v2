cb5b4d73a474bb10345574a9f225d8c8318c61f8
omaryahia4 <omar.yahia4444@gmail.com>
2021-09-12 11:07:12 +0100

    fixed

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -40,10 +40,10 @@ def number(n):
     return '{} is a number'.format(n)
 
 
-@app.route('/number_template/<int:n>')
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def template(n):
     return render_template(
-        '5-number.html')
+        '5-number.html', n=n)
 
 
 if __name__ == '__main__':
